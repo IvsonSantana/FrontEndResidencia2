3283301dddad787fcaaf2c47325642f0f17053c9
IvsonSantana <Ivson2323@gmail.com>
2024-11-20 17:05:19 -0300

    modified:   src/components/Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -24,30 +24,30 @@ const Navbar = () => {
       {/* Menu lateral */}
       <div className={`left ${isMenuActive ? 'active' : ''}`}>
         <div className="top">
-          <a className="yellow">
+          <a href="#" className="yellow">
             <span>+</span>Nova Análise
           </a>
-          <a>
+          <a href="#">
             <img src={ChatTextIcon} alt="Chat" />Nxo Ética
           </a>
-          <a>
+          <a href="#">
             <img src={ChatTextIcon} alt="Chat" />Impacto Nxo
           </a>
         </div>
         <div className="bottom">
-          <a>
+          <a href="#">
             <img src={TrashIcon} alt="Lixeira" />Apagar Análises
           </a>
-          <a >
+          <a href="#">
             <img src={IconSet} alt="Modo Claro" />Modo Claro
           </a>
-          <a >
+          <a href="#">
             <img src={UserIcon} alt="Usuário" />Minha Conta
           </a>
-          <a>
+          <a href="#">
             <img src={IconSet1} alt="Atualizações" />Atualizações
           </a>
-          <a>
+          <a href="#">
             <img src={SignOutIcon} alt="Sair" />Sair
           </a>
         </div>
